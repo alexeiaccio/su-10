0db0059fd339291b8e46251ef7e5eb9c92cf6fee
Alexei Accio <alexei.accio@gmail.com>
2018-10-12 20:35:23 +0300

    header' styles

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,26 +2,87 @@
 import React from 'react'
 /* import PropTypes from 'prop-types'  */
 import { Link } from 'gatsby'
-import styled from 'react-emotion'
+import styled, { cx, css } from 'react-emotion'
 
-import Container from './Styles'
+import { Button, Col, Container, Row } from './Styles'
 import logoSvg from '../assets/logo.svg'
+
+const Description = styled('div')`
+  ${tw(['leading-normal', 'text-base'])};
+`
+
+const HiddenToMD = css`
+  ${tw(['hidden', 'md:block'])};
+`
+
+const HiddenToSM = css`
+  ${tw(['hidden', 'sm:block'])};
+`
+
+const HiddenToXL = css`
+  ${tw(['hidden', 'xl:block'])};
+`
+
+const Logo = styled(Link)`
+  ${tw([
+    'bg-contain',
+    'bg-no-repeat',
+    'block',
+    'h-q36',
+    'sm:h-q48',
+    'md:h-q64',
+    'w-full',
+  ])}
+  background-image: url(${logoSvg});
+`
+
+const MarginLeftAuto = css`
+  ${tw(['ml-auto'])};
+`
+
+const RowItemsCentered = styled(Row)`
+  ${tw(['items-center'])};
+`
 
 const StyledHeader = styled('header')`
   ${tw(['bg-white', 'py-q20'])};
 `
 
-const Logo = styled(Link)`
-  ${tw(['block'])}
-  background-image: url(${logoSvg});
-  height: 58px;
-  width: 170px;
+const Tel = styled('a')`
+  ${tw(['font-bold', 'no-underline', 'text-black', 'text-xl'])};
+`
+
+const TelBlock = styled('div')`
+  ${tw(['leading-normal', 'text-xs'])};
 `
 
 const Header = () => (
   <StyledHeader>
     <Container>
-      <Logo to="/" />
+      <RowItemsCentered>
+        <Col number={{ xs: 6, md: 4, lg: 3, xl: 2 }}>
+          <Logo to="/" />
+        </Col>
+        <Col number={4} className={HiddenToXL}>
+          <Description>
+            Работаем в Санкт-Петербурге
+            <br />и Ленинградской области
+          </Description>
+        </Col>
+        <Col number={3} className={cx(HiddenToMD, MarginLeftAuto)}>
+          <TelBlock>
+            <Tel href="tel:+7812556678">+7 (812) 55-66-78</Tel>
+            <br />
+            Работаем ежедневно с 9 до 21
+          </TelBlock>
+        </Col>
+        <Col
+          number={{ xs: 6, md: 3 }}
+          className={cx(HiddenToSM, MarginLeftAuto)}
+        >
+          <Button>оставить заявку</Button>
+        </Col>
+      </RowItemsCentered>
     </Container>
   </StyledHeader>
 )
--- src/components/Styles.js
+++ src/components/Styles.js
@@ -1,9 +1,75 @@
 /* global tw */
+import PropTypes from 'prop-types'
 import styled from 'react-emotion'
 
+const screens = {
+  sm: '576px',
+  md: '768px',
+  lg: '992px',
+  xl: '1200px',
+}
+
+const evalWidth = number => `width: ${(100 / 12) * number}%`
+
+const objectKeystoString = obj =>
+  Object.keys(obj)
+    .filter(key => key !== 'xs')
+    .map(key => `@media (min-width: ${screens[key]}) {${evalWidth(obj[key])}}`)
+    .join(' ')
+
+const makeWidths = number =>
+  number instanceof Object
+    ? `${evalWidth(number.xs)}; ${objectKeystoString(number)}`
+    : evalWidth(number)
+
+/* Button */
+const Button = styled('button')`
+  ${tw([
+    'bg-orange',
+    'border-none',
+    'cursor-pointer',
+    'outline-none',
+    'py-q12',
+    'md:py-q16',
+    'rounded-full',
+    'text-base',
+    'uppercase',
+    'w-full',
+  ])};
+`
+
+/* Col */
+const Col = styled('div')`
+  ${tw(['px-q16'])};
+  ${({ number }) => (number ? makeWidths(number) : tw(['w-auto']))};
+`
+
+Col.propTypes = {
+  number: PropTypes.oneOfType([
+    PropTypes.number,
+    PropTypes.shape({
+      xs: PropTypes.number.isRequired,
+      sm: PropTypes.number,
+      md: PropTypes.number,
+      lg: PropTypes.number,
+      xl: PropTypes.number,
+    }),
+  ]),
+}
+
+Col.defaultProps = {
+  number: 1,
+}
+
+/* Container */
 const Container = styled('div')`
   ${tw(['mx-auto', 'max-w-container', 'px-q16', 'w-full'])};
   box-sizing: border-box;
 `
 
-export default Container
+/* Row */
+const Row = styled('div')`
+  ${tw(['flex', 'flex-row', '-mx-q16'])};
+`
+
+export { Button, Col, Container, Row }
